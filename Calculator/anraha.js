41193193cbc528f19e6070e5bf450f78d9cac4ac
Keiroru <124210421+Keiroru@users.noreply.github.com>
2024-11-12 18:55:25 +0100

    Add files via upload

--- Calculator/anraha.js
+++ Calculator/anraha.js
@@ -1,6 +1,11 @@
-let calcOn = false
-function showCalc(){
-    if (calcOn == false){
+var calcOn = false
+var second = false
+
+var numberArray = [];
+var current;
+
+function showCalculator() {
+    if (calcOn == false) {
         document.getElementById('calc').style.display = "contents";
         calcOn = true;
     }
@@ -8,4 +13,52 @@ function showCalc(){
         document.getElementById('calc').style.display = "none";
         calcOn = false;
     }
+}
+
+function getNumber(numberInput) {
+        numberArray += numberInput;
+        if (second)
+        {
+            document.getElementById('mainScreen').innerHTML = "";
+        }
+        current = numberInput
+        document.getElementById('mainScreen').innerHTML += numberInput;
+        second = false
+};
+
+function getOperator(operatoInput) {
+    numberArray += operatoInput;
+    document.getElementById('secondaryScreen').innerHTML = numberArray;
+    second = true
+}
+
+function calculate() {   
+    let value = eval(numberArray);
+    numberArray = [value];
+
+    document.getElementById('mainScreen').innerHTML = value;
+    document.getElementById('secondaryScreen').innerHTML = value;
+
+    value = [];
+};
+
+function clearCalculator() {
+    document.getElementById('mainScreen').innerHTML = "0";
+    document.getElementById('secondaryScreen').innerHTML = ""
+    value = [];
+    numberArray = [];
+}
+
+function ceClearCalculator() {
+    document.getElementById('mainScreen').innerHTML = "0";
+    value = [];
+    numberArray = [];
+}
+
+function square() {
+    let value = current * current;
+    
+    document.getElementById('mainScreen').innerHTML = value;
+    
+    value = [];
 }
